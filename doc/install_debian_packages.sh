ea90000b163b1e5ca9a5ee14296b06c77d032b3c
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-09-06 17:45:17 +0200

    MED interface removed.

--- doc/install_debian_packages.sh
+++ doc/install_debian_packages.sh
@@ -5,6 +5,4 @@ if [ $(whoami) != "root" ]; then
 	exit 1
 fi
 
-apt-get install g++ gfortran cmake libboost-dev libglib2.0-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglext1-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libhdf5-mpi-dev libmed-dev libmed-tools libmed1 libmedc-dev libmedc1 libmedimport-dev libmedimport0 libmedimportcxx-dev libsqlite3-dev libvtk5-dev libX11-dev
-
-#libmedimportcxx0 Not found in Ubuntu 14.04
+apt-get install g++ gfortran cmake libboost-dev libglib2.0-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglext1-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libsqlite3-dev libvtk5-dev libX11-dev
--- doc/install_ubuntu16_packages.sh
+++ doc/install_ubuntu16_packages.sh
@@ -5,6 +5,4 @@ if [ $(whoami) != "root" ]; then
 	exit 1
 fi
 
-apt-get install g++ gfortran cmake libboost-dev libglib2.0-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglext1-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libhdf5-mpi-dev libmed-dev libmed-tools libmed1v5 libmedc-dev libmedc1v5 libmedimport-dev libmedimport0v5 libmedimport-dev libsqlite3-dev libvtk5-dev libx11-dev
-
-#libmedimportcxx0 Not found in Ubuntu 14.04
+apt-get install g++ gfortran cmake libboost-dev libglib2.0-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglext1-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libsqlite3-dev libvtk5-dev libx11-dev
